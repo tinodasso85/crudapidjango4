1b71866999860d244ac67861136d79a8e4dbb739
Darwin Galindo <dargalindosilva@gmail.com>
2023-04-18 15:52:41 -0500

    base

---
+++ todos/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from todos import views
+
+
+urlpatterns = [
+    path('todos/', views.TodosAPIView.as_view()),
+    path('todo/<int:pk>/', views.TodoAPIView.as_view()),
+]
